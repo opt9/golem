bc212451fa90c1fd934f8caf21176aa7f22b0e22
Igor Adamski <igor@golem.network>
2017-12-27 13:29:28 +0100

    Move tokens implementations to its own file

--- tests/golem/ethereum/test_paymentprocessor_eth.py
+++ tests/golem/ethereum/test_paymentprocessor_eth.py
@@ -10,6 +10,12 @@ from golem import testutils
 from golem.ethereum import paymentprocessor
 from golem.utils import encode_hex
 
+def mock_token():
+    token = mock.Mock()
+    token.GAS_PRICE = 0
+    token.GAS_PER_PAYMENT = 0
+    token.GAS_BATCH_PAYMENT_BASE = 0
+    return token
 
 class TestPaymentProcessor(unittest.TestCase):
     def setUp(self):
@@ -18,7 +24,8 @@ class TestPaymentProcessor(unittest.TestCase):
         with mock.patch("golem.ethereum.paymentprocessor.PaymentProcessor.load_from_db"):  # noqa
             self.payment_processor = paymentprocessor.PaymentProcessor(
                 client=mock.MagicMock(),
-                privkey=privkey
+                privkey=privkey,
+                token=mock_token()
             )
 
     def test_eth_address(self):
@@ -41,7 +48,8 @@ class TestPaymentProcessorWithDB(testutils.DatabaseFixture):
         client = mock.MagicMock()
         self.payment_processor = paymentprocessor.PaymentProcessor(
             client=client,
-            privkey=privkey
+            privkey=privkey,
+            token=mock_token()
         )
 
     @mock.patch("golem.ethereum.paymentprocessor.PaymentProcessor.eth_balance", return_value=2**100)  # noqa
